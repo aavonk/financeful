0094d0cdd0532dcc9821b7af428271d051fa83bd
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-03-12 16:02:39 -0600

    fix state update on unmounted component

--- web/src/components/Layout/index.tsx
+++ web/src/components/Layout/index.tsx
@@ -9,10 +9,23 @@ function Layout({ children }: { children: React.ReactNode }) {
   const [drawerOpen, setDrawerOpen] = useState(true);
   const tabletAndDown = useMediaQuery('(max-width: 768px)');
 
+  // When loging out, the component will try to set the state
+  // on an unmounted component which causes a memory leak.
+  // useEffect will return the cleanup function when the component
+  // unmounts before it calls the effect, allowing the effect to
+  // be skipped after the component has unmounted.
+
   useEffect(() => {
-    if (tabletAndDown && drawerOpen) {
-      setDrawerOpen(!drawerOpen);
+    let isMounted = true;
+    if (isMounted) {
+      if (tabletAndDown && drawerOpen) {
+        setDrawerOpen(!drawerOpen);
+      }
     }
+
+    return () => {
+      isMounted = false;
+    };
   }, [tabletAndDown]);
 
   return (
