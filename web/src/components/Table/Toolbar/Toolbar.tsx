0bd5bfbc9d2a78aaac825c526ce80f8641cb2749
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-03-19 16:20:08 -0500

    cleanup

--- web/src/components/Table/Toolbar/Toolbar.tsx
+++ web/src/components/Table/Toolbar/Toolbar.tsx
@@ -1,7 +1,6 @@
 import { ToolbarRoot, ActionsContainer } from './style';
 import Searchbox from './Searchbox';
 import { TableInstance } from 'react-table';
-// import DatePicker from './DateRangeFilter';
 
 type ToolbarProps<T extends Record<string, unknown>> = {
   instance: TableInstance<T>;
@@ -19,7 +18,6 @@ function Toolbar<T extends Record<string, unknown>>({
           <div key={column.id}>{column.render('Filter')}</div>
         ))}
       <ActionsContainer>
-        {/* <DatePicker /> */}
         <Searchbox instance={instance} />
       </ActionsContainer>
     </ToolbarRoot>
