5fef8aca2cafca58667d613a5da48a9cdc55bb13
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-03-27 21:00:23 -0500

    no transactions display

---
+++ web/src/pages/TransactionPage/components/NoTransactions.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Paper from '@Common/Paper';
+import TransactionForm from '@Components/TransactionForm';
+import { EmptyContainer } from '../style';
+import { ViewError } from '@Components/ErrorViews';
+const NoTransactions: React.FC = () => {
+  return (
+    <Paper minHeight="450px">
+      <EmptyContainer>
+        <ViewError
+          emoji="🤑"
+          heading="No transactions yet!"
+          subheading="When you add some, you'll see them here."
+        />
+        <div style={{ marginTop: '1rem' }}>
+          <TransactionForm />
+        </div>
+      </EmptyContainer>
+    </Paper>
+  );
+};
+export default NoTransactions;
--- web/src/pages/TransactionPage/style.ts
+++ web/src/pages/TransactionPage/style.ts
@@ -4,3 +4,12 @@ export const TableContainer = styled.div`
   width: 100%;
   margin-top: 1.5rem;
 `;
+
+export const EmptyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1 0 auto;
+  margin-top: 8rem;
+`;
