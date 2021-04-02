7cafcab51d726b29943f476021f20c80a220b021
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-04-02 11:12:40 -0500

    add notes

--- web/src/common/Alerts/Toast.tsx
+++ web/src/common/Alerts/Toast.tsx
@@ -7,6 +7,28 @@ interface AlertProps {
   message: string;
 }
 
+/* 
+  This Component looks exactly the same as the Alert component. 
+  This is to be used when we want to return an actual component,
+  rather than using the showAlert function provided by the alert context. 
+
+  A good example of when to use this rather than using the alert context
+  is when catching an error from a graphql query
+
+  ex: BAD: 
+      if (error) {
+        showAlert('some message', 'error')
+        
+      }
+      *** This causes an infinite loop of rerenders
+  
+  ex: GOOD:
+      if (error) {
+        return <Toast type="error" message="Some message" />
+      }
+      *** The error will dissapear after three seconds
+*/
+
 function Toast({ type, message }: AlertProps) {
   const [show, setShow] = useState(true);
 
