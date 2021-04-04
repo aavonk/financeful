19abc55edbaf41864507bc8773c8fa8e2aefc2f9
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-04-04 18:34:20 -0500

    refactor to hook that updates cache

--- web/src/modules/transactions/Forms/TransactionForm/index.tsx
+++ web/src/modules/transactions/Forms/TransactionForm/index.tsx
@@ -4,24 +4,25 @@ import { useState } from 'react';
 import { Overlay, Content } from '../style';
 import Button from '@Common/Button';
 import { useMediaQuery } from '@Hooks/useMediaQuery';
-import PaymentForm from './PaymentForm';
 import {
   useFetchAccountsAndCategoriesQuery,
-  useAddTransactionMutation,
-  GetTransactionsDocument,
   TransactionInput,
   TransferInput,
 } from '@Generated/graphql';
 import { useAlert } from '@Context/alert/alertContext';
 import { Form } from './FormProvider';
+import PaymentForm from './PaymentForm';
 import TransferForm from './TransferForm';
 import { useCreateTransfer } from '../../mutations/useCreateTransfer';
+import { useCreateTransaction } from '../../mutations/useCreateTransaction';
 
 function TransactionForm() {
   const { data, loading, error } = useFetchAccountsAndCategoriesQuery();
-  const [addTransaction, submitting] = useAddTransactionMutation();
   const { mutate: createTransfer, loading: submittingTransfer } = useCreateTransfer();
-
+  const {
+    mutate: createTransaction,
+    loading: submittingTransaction,
+  } = useCreateTransaction();
   const [showDialog, setShowDialog] = useState(false);
   const smallDevice = useMediaQuery('(max-width: 605px)');
   const { showAlert } = useAlert();
@@ -30,22 +31,7 @@ function TransactionForm() {
   const close = () => setShowDialog(false);
 
   const onPaymentSubmit = async (values: TransactionInput) => {
-    const response = await addTransaction({
-      variables: { input: values },
-      update: (cache, { data: createTransaction }) => {
-        cache.modify({
-          fields: {
-            getTransactions: (existingFieldData = []) => {
-              const newTransactionRef = cache.writeQuery({
-                data: createTransaction,
-                query: GetTransactionsDocument,
-              });
-              return [newTransactionRef, ...existingFieldData];
-            },
-          },
-        });
-      },
-    });
+    const response = await createTransaction({ variables: { input: values } });
 
     if (response.data?.createTransaction) {
       showAlert('Transaction successfully added', 'info');
@@ -82,17 +68,16 @@ function TransactionForm() {
                 onFormSubmit={onPaymentSubmit}
                 categories={data?.getCategories}
                 accounts={data?.getAccounts}
-                isSubmitting={submitting.loading}
+                isSubmitting={submittingTransaction}
               />
             </Form.Payment>
             <Form.Transfer>
               <TransferForm
                 onFormSubmit={onTransferSubmit}
                 accounts={data?.getAccounts}
-                isSubmitting={false}
+                isSubmitting={submittingTransfer}
                 categories={data?.getCategories}
               />
-              {/* Remember to change isSubmitting prop */}
             </Form.Transfer>
             <Form.ErrorView />
           </Form>
---
+++ web/src/modules/transactions/mutations/useCreateTransaction.tsx
@@ -0,0 +1,21 @@
+import { useAddTransactionMutation, GetTransactionsDocument } from '@Generated/graphql';
+
+export function useCreateTransaction() {
+  const [mutate, { data, error, loading }] = useAddTransactionMutation({
+    update(cache, { data }) {
+      cache.modify({
+        fields: {
+          getTransactions: (existingFieldData = []) => {
+            const newTransactionRef = cache.writeQuery({
+              data: data?.createTransaction,
+              query: GetTransactionsDocument,
+            });
+            return [newTransactionRef, ...existingFieldData];
+          },
+        },
+      });
+    },
+  });
+
+  return { mutate, data, error, loading };
+}
