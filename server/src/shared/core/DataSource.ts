1857474e13fb851acdb7f7bae59662348ea96313
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-04-10 15:33:27 -0500

    make db client protexted

--- server/src/shared/core/DataSource.ts
+++ server/src/shared/core/DataSource.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import prisma from '../database/prisma';
 
 export abstract class DataSource {
-  public client: PrismaClient;
+  protected client: PrismaClient;
   constructor() {
     this.client = prisma;
   }
