65dfcee8819e1c60a81cc02686a23875d54b887b
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-04-13 16:53:57 -0500

    set up account list with containers that fetch data

--- web/src/modules/wallet/Forms/AddAccountForm.tsx
+++ web/src/modules/wallet/Forms/AddAccountForm.tsx
@@ -1,4 +1,15 @@
-function AddAccountForm() {
+import { BorderedInput, ErrorMessage } from '@Common/FormElements';
+import { Row, Col } from '@Globals/index';
+import { useForm } from '@Hooks/useForm';
+
+type AddAccountProps = {
+  onFormSubmit: () => void;
+};
+
+const initialState = {
+  bankName: '',
+};
+function AddAccountForm({ onFormSubmit }: AddAccountProps) {
   return <div>Form</div>;
 }
 
