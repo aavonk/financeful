025896ec1b20cba1d4be5c1227416c6e9162dd59
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-04-15 17:33:10 -0500

    import modules for seed script

---
+++ server/prisma/script.ts
@@ -0,0 +1,28 @@
+// const chalk = require('chalk')
+// import ask from 'prompt'
+// import {PrismaClient} from '@prisma/client'
+// import { AuthRepo } from '../src/modules/Auth/repos/implementations/authRepo'
+// import { RegisterInput } from '../src/modules/Auth/resolvers/types'
+
+// // import { accounts} from './seed-data/accounts'
+// // import {categories} from './seed-data/categories'
+
+// const prisma = new PrismaClient()
+// const auth = new AuthRepo()
+
+// async function script(){
+//   console.log(chalk.blue('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n'))
+//   console.log(chalk`Welcome to {blue.bold Financeful}. Lets get started by seeding the database. \n`);
+//   console.log('First let\'s create an account...')
+//   ask.start()
+//   const name: = await ask.get(['name'])
+//   const email = await ask.get(['email'])
+//   const password = await ask.get(['password'])
+//   const confirmPassword = await ask.get(['confirmPassword'])
+  
+
+//   // const registerInput: RegisterInput = {displayName: name, email, }
+
+// }
+
+// script()
