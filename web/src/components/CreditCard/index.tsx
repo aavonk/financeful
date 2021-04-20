836d63109c1c0c19618456c850c00da805b5baf4
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-04-20 11:28:46 -0500

    overflowing text

--- web/src/components/CreditCard/index.tsx
+++ web/src/components/CreditCard/index.tsx
@@ -2,7 +2,7 @@
 import { CSSProperties } from 'react';
 import { formatMoneyFromCentsToDollars } from '@Lib/money-utils';
 import { Account } from '@Generated/graphql';
-import { Card, BankName, Data, Chip } from './style';
+import { Card, BankName, Data, Chip, AmountBar } from './style';
 
 type Props = {
   style?: CSSProperties;
@@ -13,17 +13,23 @@ function CreditCard({ style, account }: Props) {
   return (
     <Card style={style}>
       <BankName>
-        <span />
-        <p>{account.accountName}</p>
+        <span className="empty" />
+        <div className="names">
+          <p>{account.accountName}</p>
+          <span className="bank-name">{account.bankName}</span>
+        </div>
       </BankName>
-      <Chip>
-        <div className="side left" />
-        <div className="side right" />
-        <div className="vertical top" />
-        <div className="vertical bottom" />
-      </Chip>
-      <Data>
+      <AmountBar>
+        <Chip>
+          <div className="side left" />
+          <div className="side right" />
+          <div className="vertical top" />
+          <div className="vertical bottom" />
+        </Chip>
         <div className="pan"> {formatMoneyFromCentsToDollars(account.balance!)}</div>
+      </AmountBar>
+
+      <Data>
         <div className="details">
           <div className="label">Last used</div>
           <div className="date">12/24/2021</div>
--- web/src/components/CreditCard/style.ts
+++ web/src/components/CreditCard/style.ts
@@ -19,18 +19,51 @@ export const BankName = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
-  & > span {
+  & > span.empty {
     flex-basis: 20%;
+    width: 20px;
   }
-  & > p {
-    margin-top: 15px;
-    margin-right: 30px;
+
+  & .names {
+    /* display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    align-items: flex-end; */
+    margin-right: 20px;
+    margin-top: 10px;
+    overflow: hidden;
+    text-align: right;
+  }
+  & > .names > p {
     font-size: 20px;
     font-weight: 600;
-    float: right;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
+  }
+
+  & > .names > span.bank-name {
+    color: ${({ theme }) => theme.colors.textSecondary};
+    font-size: 14px;
+  }
+`;
+
+export const AmountBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-left: 20px;
+  margin-right: 20px;
+
+  & > .pan {
+    position: relative;
+    /* z-index: 2; */
+    letter-spacing: 1px;
+    text-shadow: 1px 1px 1px #000;
+    font-size: 22px;
+    font-weight: 700;
   }
 `;
 
@@ -39,9 +72,8 @@ export const Chip = styled.div`
   z-index: 10;
   width: 40px;
   height: 30px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 30px;
+  margin-top: 6px;
+  margin-bottom: 6px;
   background: #fffcb1;
 
   /* Old browsers */
@@ -184,15 +216,14 @@ export const Chip = styled.div`
 export const Data = styled.div`
   position: relative;
   z-index: 3;
-  margin-left: 30px;
-  & > .pan {
+  margin-left: 20px;
+  /* & > .pan {
     position: relative;
-    /* z-index: 2; */
     letter-spacing: 1px;
     text-shadow: 1px 1px 1px #000;
     font-size: 22px;
     font-weight: 700;
-  }
+  } */
 
   & > .details {
     margin-top: 5px;
@@ -208,271 +239,5 @@ export const Data = styled.div`
     font-size: 12px;
     letter-spacing: 1px;
     text-shadow: 1px 1px 1px #000;
-  }
-`;
-
-export const LinesUp = styled.div`
-  :before {
-    content: '';
-    position: absolute;
-    top: -110px;
-    left: -70px;
-    z-index: 2;
-    width: 480px;
-    height: 300px;
-    border-bottom: 2px solid #392db2;
-    -webkit-border-radius: 0 0 60% 90%;
-    -webkit-background-clip: padding-box;
-    -moz-border-radius: 0 0 60% 90%;
-    -moz-background-clip: padding;
-    border-radius: 0 0 60% 90%;
-    background-clip: padding-box;
-    box-shadow: inset 1px 1px 44px #4035b2;
-    background: #4031b2;
-    /* Old browsers */
-    background: -moz-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(64, 49, 178, 0) 100%,
-      #2271d8 100%
-    );
-    /* FF3.6+ */
-    background: -webkit-gradient(
-      radial,
-      center center,
-      0px,
-      center center,
-      100%,
-      color-stop(0%, rgba(64, 49, 178, 0)),
-      color-stop(100%, #2271d8)
-    );
-    /* Chrome,Safari4+ */
-    background: -webkit-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(64, 49, 178, 0) 100%,
-      #2271d8 100%
-    );
-    /* Chrome10+,Safari5.1+ */
-    background: -o-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(64, 49, 178, 0) 44%,
-      #2271d8 100%
-    );
-    /* Opera 12+ */
-    background: -ms-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(64, 49, 178, 0) 44%,
-      #2271d8 100%
-    );
-    /* IE10+ */
-    background: radial-gradient(
-      ellipse at center,
-      rgba(64, 49, 178, 0) 44%,
-      #2271d8 100%
-    );
-    /* W3C */
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="rgba(64, 49, 178, 0)", endColorstr="#2271d8", GradientType=1);
-    /* IE6-9 fallback on horizontal gradient */
-  }
-  :after {
-    content: '';
-    position: absolute;
-    top: -180px;
-    left: -200px;
-    z-index: 1;
-    width: 530px;
-    height: 420px;
-    border-bottom: 2px solid #4035b2;
-    -webkit-border-radius: 0 40% 50% 50%;
-    -webkit-background-clip: padding-box;
-    -moz-border-radius: 0 40% 50% 50%;
-    -moz-background-clip: padding;
-    border-radius: 0 40% 50% 50%;
-    background-clip: padding-box;
-    box-shadow: inset 1px 1px 44px #4035b2;
-    background: #1e88e5;
-    /* Old browsers */
-    background: -moz-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 100%,
-      #1e88e5 100%
-    );
-    /* FF3.6+ */
-    background: -webkit-gradient(
-      radial,
-      center center,
-      0px,
-      center center,
-      100%,
-      color-stop(0%, rgba(45, 33, 166, 0)),
-      color-stop(100%, #1e88e5)
-    );
-    /* Chrome,Safari4+ */
-    background: -webkit-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 100%,
-      #1e88e5 100%
-    );
-    /* Chrome10+,Safari5.1+ */
-    background: -o-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* Opera 12+ */
-    background: -ms-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* IE10+ */
-    background: radial-gradient(
-      ellipse at center,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* W3C */
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="rgba(45, 33, 166, 0)", endColorstr="#1e88e5", GradientType=1);
-    /* IE6-9 fallback on horizontal gradient */
-  }
-`;
-
-export const LinesDown = styled.div`
-  :before {
-    content: '';
-    position: absolute;
-    top: 80px;
-    left: -200px;
-    z-index: 2;
-    width: 550px;
-    height: 400px;
-    border-top: 2px solid #392db2;
-    -webkit-border-radius: 40% 60% 0 0;
-    -webkit-background-clip: padding-box;
-    -moz-border-radius: 40% 60% 0 0;
-    -moz-background-clip: padding;
-    border-radius: 40% 60% 0 0;
-    background-clip: padding-box;
-    box-shadow: 1px 1px 100px #4035b2;
-    background: #1e88e5;
-    /* Old browsers */
-    background: -moz-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 100%,
-      #1e88e5 100%
-    );
-    /* FF3.6+ */
-    background: -webkit-gradient(
-      radial,
-      center center,
-      0px,
-      center center,
-      100%,
-      color-stop(0%, rgba(45, 33, 166, 0)),
-      color-stop(100%, #1e88e5)
-    );
-    /* Chrome,Safari4+ */
-    background: -webkit-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 100%,
-      #1e88e5 100%
-    );
-    /* Chrome10+,Safari5.1+ */
-    background: -o-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* Opera 12+ */
-    background: -ms-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* IE10+ */
-    background: radial-gradient(
-      ellipse at center,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* W3C */
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="rgba(45, 33, 166, 0)", endColorstr="#1e88e5", GradientType=1);
-    /* IE6-9 fallback on horizontal gradient */
-  }
-  :after {
-    content: '';
-    position: absolute;
-    top: 20px;
-    left: -100px;
-    z-index: 2;
-    width: 350px;
-    height: 400px;
-    border-top: 2px solid #392db2;
-    -webkit-border-radius: 20% 80% 0 0;
-    -webkit-background-clip: padding-box;
-    -moz-border-radius: 20% 80% 0 0;
-    -moz-background-clip: padding;
-    border-radius: 20% 80% 0 0;
-    background-clip: padding-box;
-    box-shadow: inset -1px -1px 44px #4035b2;
-    background: #1e88e5;
-    /* Old browsers */
-    background: -moz-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 100%,
-      #1e88e5 100%
-    );
-    /* FF3.6+ */
-    background: -webkit-gradient(
-      radial,
-      center center,
-      0px,
-      center center,
-      100%,
-      color-stop(0%, rgba(45, 33, 166, 0)),
-      color-stop(100%, #1e88e5)
-    );
-    /* Chrome,Safari4+ */
-    background: -webkit-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 100%,
-      #1e88e5 100%
-    );
-    /* Chrome10+,Safari5.1+ */
-    background: -o-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* Opera 12+ */
-    background: -ms-radial-gradient(
-      center,
-      ellipse cover,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* IE10+ */
-    background: radial-gradient(
-      ellipse at center,
-      rgba(45, 33, 166, 0) 44%,
-      #1e88e5 100%
-    );
-    /* W3C */
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="rgba(45, 33, 166, 0)", endColorstr="#1e88e5", GradientType=1);
-    /* IE6-9 fallback on horizontal gradient */
   }
 `;
