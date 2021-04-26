d2fc28d5b497fe38260877b69e3be64870c1c097
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-04-26 18:13:27 -0500

    prisma logging

--- server/src/shared/database/prisma.ts
+++ server/src/shared/database/prisma.ts
@@ -1,5 +1,31 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma = new PrismaClient();
+let prisma = new PrismaClient({
+  errorFormat: 'pretty',
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  console.log('Query: ' + e.query);
+  console.log('Params: ' + e.params);
+  console.log('Duration: ' + e.duration + 'ms');
+});
 
 export default prisma;
