2c171a082e58587241dbbdf95bbffc3698135b99
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-05-05 19:10:14 -0500

    function that returns a series of date ranges

--- web/src/lib/date-formatting.ts
+++ web/src/lib/date-formatting.ts
@@ -1,4 +1,14 @@
-import { format, add, Duration } from 'date-fns';
+import {
+  format,
+  add,
+  Duration,
+  endOfMonth,
+  startOfMonth,
+  subMonths,
+  endOfDay,
+  startOfYear,
+  endOfYear,
+} from 'date-fns';
 
 type FormatOptions = 'MMM do yyyy' | 'M/d/yyyy';
 
@@ -12,4 +22,87 @@ export const addDays = (date: Date, duration: Duration) => {
   // of the hour/minute
   date.setHours(0, 0, 0, 0);
   return add(date, duration);
+};
+
+export const getEndOfMonth = (date: Date): Date => {
+  return endOfMonth(date);
+};
+
+const getStartOfMonth = (date: Date): Date => {
+  return startOfMonth(date);
+};
+
+const subtractMonths = (date: Date, amount: number): Date => {
+  return subMonths(date, amount);
+};
+
+const getEndOfDay = (date: Date): Date => {
+  return endOfDay(date);
+};
+
+const getStartOfYear = (date: Date): Date => {
+  return startOfYear(date);
+};
+
+const getEndOfYear = (date: Date): Date => {
+  return endOfYear(date);
+};
+
+type RangeOptions =
+  | 'current-month'
+  | 'last-month'
+  | '90-days'
+  | '1-year'
+  | 'this-year'
+  | 'last-year';
+
+type Range = {
+  startDate: Date;
+  endDate: Date;
+};
+
+//
+export const getDateRange = (range: RangeOptions): Range => {
+  const today = new Date();
+
+  switch (range) {
+    case 'current-month': {
+      const endDate = getEndOfDay(getEndOfMonth(today));
+      const startDate = getStartOfMonth(today);
+      return { startDate, endDate };
+    }
+    case 'last-month': {
+      const startMonth = getStartOfMonth(today);
+      const startDate = subtractMonths(startMonth, 1);
+      const endDate = getEndOfDay(getEndOfMonth(startDate));
+      return { startDate, endDate };
+    }
+    case '90-days': {
+      return {
+        startDate: addDays(today, { days: -90 }),
+        endDate: getEndOfDay(today),
+      };
+    }
+    case '1-year': {
+      return {
+        startDate: addDays(today, { years: -1 }),
+        endDate: getEndOfDay(today),
+      };
+    }
+    case 'this-year': {
+      return {
+        startDate: getStartOfYear(today),
+        endDate: getEndOfDay(today),
+      };
+    }
+    case 'last-year': {
+      const oneYearAgo = addDays(today, { years: -1 });
+      return {
+        startDate: getStartOfYear(oneYearAgo),
+        endDate: getEndOfYear(oneYearAgo),
+      };
+    }
+    default:
+      throw new Error('Insufficient date range passed to getDateRange');
+  }
 };
