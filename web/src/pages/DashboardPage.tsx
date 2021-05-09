5459759eebb8fa84e0c09a4dd087bef3b3997e2b
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-05-08 21:57:29 -0500

    change balance overview skeleton

--- web/src/pages/DashboardPage.tsx
+++ web/src/pages/DashboardPage.tsx
@@ -15,9 +15,6 @@ function DashboardPage() {
             Change Y Axis on areas to include low and high value, using type number and
             domain prop
           </li>
-          <li>
-            Update Assets & Liabilites Card & cache when account is created or deleted
-          </li>
         </ul>
       </div>
     </>
