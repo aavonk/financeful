19109321a3657dbac89b81fc9bd1838d41435807
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-05-08 22:00:13 -0500

    remove unused modules

--- web/src/pages/DashboardPage.tsx
+++ web/src/pages/DashboardPage.tsx
@@ -1,16 +1,11 @@
-import CardLoader from '@Components/CreditCard/CardLoader';
-
 function DashboardPage() {
   return (
     <>
       <p>Hi</p>
-      <CardLoader />
       <div style={{ marginTop: '20px', maxWidth: '800px' }}></div>
       <div>
         TODO:
         <ul>
-          <li>Delete CreditCards and CreditCards controller</li>
-          <li>Delete Account Overview Modal</li>
           <li>
             Change Y Axis on areas to include low and high value, using type number and
             domain prop
