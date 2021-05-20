07986c14b95a0e5f45e30c549c757d0daffadc6c
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-05-20 08:57:23 -0500

    category repo test

---
+++ server/src/__tests__/__mocks__/fixtures/categories.ts
@@ -0,0 +1,46 @@
+export const categories = [
+  {
+    id: 'ckorlgy5t01343sqs8zu3w1pl',
+    name: 'Groceries',
+  },
+  {
+    id: 'ckorlgy6001403sqsb3rshouk',
+    name: 'Drinks & Snacks',
+  },
+  {
+    id: 'ckorlgy6601473sqscv8vkbch',
+    name: 'Rent',
+  },
+  {
+    id: 'ckorlgy6c01533sqs9q3qf0lg',
+    name: 'Utilities',
+  },
+  {
+    id: 'ckorlgy6h01593sqsjhfx90bf',
+    name: 'Entertainment',
+  },
+  {
+    id: 'ckorlgy6l01663sqs04hjr1eg',
+    name: 'Pets/Pet Care',
+  },
+  {
+    id: 'ckorlgy6p01733sqs9f40x2o9',
+    name: 'Electronics',
+  },
+  {
+    id: 'ckorlgy6v01793sqs9hiwsaav',
+    name: 'Transfers',
+  },
+  {
+    id: 'ckorlgy7001863sqsb5zyvct7',
+    name: 'Savings',
+  },
+  {
+    id: 'ckorlgy7601923sqse08nixjs',
+    name: 'Paycheck',
+  },
+  {
+    id: 'ckorlgy7d01993sqs223y3byk',
+    name: 'Investment Income',
+  },
+];
---
+++ server/src/__tests__/unit/Transactions/categoryRepo.test.ts
@@ -0,0 +1,85 @@
+import { MockContext, Context, createMockContext } from '../../../testSetup';
+import { CategoryRepo } from '@Modules/Transactions/repos/implementations/categoryRepo';
+import { categories } from '../../__mocks__/fixtures';
+
+let ctx: Context;
+let mock: MockContext;
+let repo: CategoryRepo;
+
+const USER_ID = 'userId';
+
+const getCategories = () => {
+  return categories.map((c) => ({ ...c, userId: USER_ID }));
+};
+
+const getOneCategory = (index: number) => {
+  return categories[index];
+};
+
+beforeEach(() => {
+  mock = createMockContext();
+  ctx = (mock as unknown) as Context;
+  repo = new CategoryRepo(ctx.prisma);
+});
+
+describe('Category Repo CRUD Operations', () => {
+  it('findAll', async () => {
+    const categories = getCategories();
+    mock.prisma.category.findMany.mockResolvedValue(categories);
+
+    await expect(repo.findAll(USER_ID)).resolves.toEqual(categories);
+  });
+
+  it('findExisting returns category', async () => {
+    const category = { ...getOneCategory(1), userId: USER_ID };
+
+    mock.prisma.category.findFirst.mockResolvedValue(category);
+
+    await expect(repo.findExisting(USER_ID, category.name)).resolves.toEqual(
+      category,
+    );
+  });
+
+  it('findExisting returns null if not found', async () => {
+    mock.prisma.category.findFirst.mockResolvedValue(null);
+    await expect(repo.findExisting(USER_ID, 'some name')).resolves.toEqual(
+      null,
+    );
+  });
+
+  it('createOne', async () => {
+    const data = { name: 'New Category', userId: USER_ID, id: '123' };
+
+    mock.prisma.category.create.mockResolvedValue(data);
+
+    await expect(repo.createOne(USER_ID, 'New Category')).resolves.toEqual({
+      name: 'New Category',
+      id: '123',
+      userId: USER_ID,
+    });
+  });
+
+  it('updateOne', async () => {
+    const updated = {
+      ...getOneCategory(2),
+      userId: USER_ID,
+      name: 'Updated Category',
+    };
+
+    mock.prisma.category.update.mockResolvedValue(updated);
+
+    await expect(
+      repo.updateOne(updated.id, 'Updated Category'),
+    ).resolves.toEqual({
+      id: updated.id,
+      name: 'Updated Category',
+      userId: USER_ID,
+    });
+  });
+
+  it('deleteOne', async () => {
+    const category = getOneCategory(1);
+
+    await expect(repo.deleteOne(category.id)).resolves.not.toThrow();
+  });
+});
