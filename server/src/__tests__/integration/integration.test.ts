9cf8fba619c82c75d2d351bf66fefeeb7ead9c32
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-05-23 10:44:23 -0500

    setup docker for integration tests with seperate db

--- server/src/__tests__/integration/integration.test.ts
+++ server/src/__tests__/integration/integration.test.ts
@@ -1,10 +1,18 @@
 import { UserRepo } from '@Modules/Users/repos/implementations/userRepo'
-import prisma from '@Shared/database/prisma'
+// import prisma from '@Shared/database/prisma'
+import { PrismaClient } from '@prisma/client'
 
 let userRepo: UserRepo
+let prisma: PrismaClient
 
 beforeAll(() => {
+  prisma = new PrismaClient()
   jest.setTimeout(10000);
+  process.env.DATABASE_URL="postgresql://prisma:prisma@localhost:5433/tests"
+})
+
+afterAll(() => {
+  prisma.$disconnect()
 })
 
 beforeEach(() => {
